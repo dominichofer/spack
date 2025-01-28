d53116afefcd64062e279061531d507af5c1f7ad
Dominic Hofer <6570912+dominichofer@users.noreply.github.com>
2025-01-28 14:27:05 +0100

    code-server: Add 4.89.1

--- var/spack/repos/builtin/packages/code-server/package.py
+++ var/spack/repos/builtin/packages/code-server/package.py
@@ -12,6 +12,7 @@ class CodeServer(Package):
     homepage = "https://coder.com/docs/code-server/latest"
     url = "https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-amd64.tar.gz"
 
+    version("4.89.1", sha256="5c3769b1ab5cbb2eceb092524dc46f558905e4260155b477d3a313f9ea25ca33")
     version("4.12.0", sha256="d50ee947c4144a6ff2656e664ecbb3f70b75168b8a6e8c3eef47787f3c240c26")
     version("4.11.0", sha256="4eb233054941ec298caec6fc84dfba0a72c1bc5fadc0fe4896b10f3f4a291d51")
     version("4.10.1", sha256="f34ce611a9c058982a5e9d200fdf009788e3a564e970b053f4145574bce21b09")
